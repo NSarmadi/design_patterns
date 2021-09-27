7997d3fd87b5b045d37229d1fb7dd79df1eebfc3
Neekon Sarmadi <nsarmadi@email.sc.edu>
2021-09-26 23:55:03 -0400

    error where comments did not push, trying to resolve

--- iterator/Flight.java
+++ iterator/Flight.java
@@ -1,3 +1,6 @@
+/**
+ * @author Neekon Sarmadi
+ */
 public class Flight {
     private String flightNum;
     private String from;
@@ -5,6 +8,14 @@ public class Flight {
     private int duration = 0;
     private int transfers = 0;
 
+    /**
+     * Constructor that calls previously enstantiated objects. 
+     * @param flightNum string value that holds the flight number
+     * @param from string value that holds the from location
+     * @param to string value that holds the to location
+     * @param duration int value that holds the duration of the flight
+     * @param transfers int value that holds the amount of transfers
+     */
     public Flight(String flightNum, String from, String to, int duration, int transfers){
         this.flightNum = flightNum;
         this.from = from;
@@ -12,18 +23,38 @@ public class Flight {
         this.duration = duration;
         this.transfers = transfers;
     }
+    /**
+     * Method to get the from location
+     * @return returns the from location
+     */
     public String getFrom(){
         return this.from;
     }
+    /**
+     * Method to return the to location
+     * @return returns the to location
+     */
     public String getTo(){
         return this.to;
     }
+    /**
+     * Method to get the duration of the flight
+     * @return returns the duration 
+     */
     public int getDuration(){
         return this.duration;
     }
+    /**
+     * Method to get number of transfers
+     * @return returns the amount of transfers
+     */
     public int getNumTransfers(){
         return this.transfers;
     }
+    /**
+     * Method to return all the values in the correct syntax.
+     * @return temp returns the seperated portion of the returned string
+     */
     public String toString(){
         String temp = "Flight Number: " + this.flightNum +"\n"+ "From: " + this.from + "\n" + "To: " + this.to + "\n" + "Duration: " + this.duration/60 + " hours " + this.duration%60 + " minutes \n";
         if(transfers >= 2){
